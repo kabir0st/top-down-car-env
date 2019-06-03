1f114a826b1d1631a7623cff47a706b2b9c8f86b
Kabir Tamari <rabbit.bonex@gmail.com>
2019-06-03 12:39:52 -0700

    simple map created done, life-improvement required

--- make_maker.py
+++ make_maker.py
@@ -15,37 +15,75 @@ class Game(object):
         self.exit = False
 
     def run(self):
-        state = 0
+        self.current_line_start = 0.0
+        self.current_line_end = 0.0
+        new_line = True
         self.map_coordinates = []
-        start = []
-        end = []
-        # current_dir = os.path.dirname(os.path.abspath(__file__))
-        
+
         while not self.exit:
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.exit = True
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        self.exit = True
+                        print(new_line)
+                    if not new_line:
+                        if event.key == K_n:
+                                print("Creating new sequences of lines.")
+                                new_line = True
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    print('Mouse at : ',event.pos)
-                    if state == 0:
-                        start = event.pos
-                        state = 1
-                    elif state == 1:
-                        end = event.pos
-                        state = 2
-            if state == 2:
-                print("saving starting point ",start," and end point ",end)
-                self.map_coordinates.append((start,end))
-                print(self.map_coordinates)
-                state = 0
+                    if new_line:
+                        self.current_line_start = event.pos
+                        print(self.current_line_start, event.pos)
+                        new_line = False
+                    else:
+                        self.current_line_end = event.pos
+                        self.map_coordinates.append((self.current_line_start,self.current_line_end))
+                        self.current_line_start = event.pos
+                if not new_line: 
+                    if event.type == pygame.MOUSEMOTION:
+                        pygame.draw.line(self.screen, (100,100,100), self.current_line_start, event.pos, 3)
             self.update()
+
+
+
+        # while not self.exit:
+
+        #     for event in pygame.event.get():
+        #         if not new_line:
+        #             pygame.draw.line(self.screen, (100,100,100),self.current_line_start,event.pos,3)
+        #         if event.type == pygame.QUIT:
+        #             self.exit = True
+        #         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        #             print('Mouse at : ',event.pos)
+        #             if state == 0:
+        #                 start = event.pos
+        #                 state = 1
+        #             elif state == 1:
+        #                 end = event.pos
+        #                 state = 2
+        #         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+        #             try:
+        #                 x = self.map_coordinates.pop()
+        #                 print(x," point removed.")
+        #             except:
+        #                 print("Array is empty.")
+                    
+        #     if state == 2:
+        #         print("saving starting point ",start," and end point ",end)
+        #         self.map_coordinates.append((start,end))
+        #         print(self.map_coordinates)
+        #         state = 0
+        #     self.update()
 
     def update(self):
         self.screen.fill((0,0,0))
         for coordinates in self.map_coordinates:
             start = coordinates[0]
             end = coordinates[1]
-            pygame.draw.line(self.screen, (255,0,0),start,end,5)
+            pygame.draw.line(self.screen, (255,0,0),start,end,3)
         pygame.display.flip()
         
 
